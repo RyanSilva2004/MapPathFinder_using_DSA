0ea53da23e6d9a8889eb4fc2f931d21f9756fef5
Ayesh Irunda <cohndse232f-100@student.nibm.lk>
2024-02-20 00:09:18 +0530

    Created Methods in class Graph

--- MapPathFinderJava/src/Main.java
+++ MapPathFinderJava/src/Main.java
@@ -5,6 +5,35 @@ public class Main {
 }
 class Graph
 {
+    City[] cities;
+    Path[] paths;
+    int citycount;
+    int pathcount;
+
+    public Graph(City[] cities, Path[] paths)
+    {
+        this.cities=cities;
+        this.paths=paths;
+        citycount = 0;
+        pathcount = 0;
+    }
+
+    public void addCity(City city_id, City city_name)
+    {
+        City city = new City();
+        city.city_id = city_id;
+        city.city_name = city_name;
+        cities[citycount] = city;
+        citycount++;
+    }
+    public void addPath(String path_id, City city1, City city2, double distance)
+    {
+        Path path = new Path(city1,city2,distance);
+        paths[pathcount] = path;
+        pathcount++;
+    }
+
+
 
 }
 class City
