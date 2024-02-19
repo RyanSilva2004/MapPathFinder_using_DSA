bdf826302f1748d78cc88034688577d8922b9aab
Vihanga Deshan Sammandapperuma <vdeshan044@gmail.com>
2024-02-19 23:56:35 +0530

    Class definition of garph

--- MapPathFinderJava/src/Main.java
+++ MapPathFinderJava/src/Main.java
@@ -3,3 +3,24 @@ public class Main {
         System.out.println("Hello world!");
     }
 }
+class Graph
+{
+
+}
+class City
+{
+    String city_id;
+    String city_name;
+}
+class Path
+{
+    String path_id;
+    City city1;
+    City city2;
+    double distance;
+    public Path(City city1,City city2, double distance)
+    {
+        this.distance=distance;
+    }
+
+}
