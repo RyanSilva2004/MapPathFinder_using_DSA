a23ef566396ab6bdfbf3ee5f170253da32e6442f
RyanSilva2004 <ryansilvamic2004@gmail.com>
2024-02-20 01:10:01 +0530

    Location Graph Logic  Implemented ( Low Level )

--- MapPathFinderJava/src/Main.java
+++ MapPathFinderJava/src/Main.java
@@ -1,55 +1,129 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of cities:");
+        int numCities = scanner.nextInt();
+        LocationGraph graph = new LocationGraph(numCities);
+
+        while (true) {
+            System.out.println("Enter 1 to add a city, 2 to add a path, 3 to display the graph, or 4 to exit:");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // consume newline left-over
+            if (choice == 1) {
+                System.out.println("Enter the city ID:");
+                String cityId = scanner.nextLine();
+                System.out.println("Enter the city name:");
+                String cityName = scanner.nextLine();
+                City city = new City(cityId, cityName);
+                graph.addCity(city);
+            } else if (choice == 2) {
+                System.out.println("Enter the first city ID:");
+                String cityId1 = scanner.nextLine();
+                System.out.println("Enter the second city ID:");
+                String cityId2 = scanner.nextLine();
+                System.out.println("Enter the distance:");
+                double distance = scanner.nextDouble();
+                scanner.nextLine(); // consume newline left-over
+                System.out.println("Is the path available? (yes/no)");
+                String isAvailableStr = scanner.nextLine();
+                boolean isAvailable = isAvailableStr.equalsIgnoreCase("yes");
+                Path path = new Path(new City(cityId1, ""), new City(cityId2, ""), distance, isAvailable);
+                graph.addPath(path);
+            } else if (choice == 3) {
+                graph.display();
+            } else if (choice == 4) {
+                break;
+            }
+        }
+
+        scanner.close();
     }
 }
-class Graph
-{
-    City[] cities;
-    Path[] paths;
-    int citycount;
-    int pathcount;
 
-    public Graph(City[] cities, Path[] paths)
-    {
-        this.cities=cities;
-        this.paths=paths;
-        citycount = 0;
-        pathcount = 0;
+//Map For Locations implemented using a Graph
+class LocationGraph {
+    double[][] adjacencyMatrix;
+    ArrayList<City> cities;
+
+    public LocationGraph(int numCities) {
+        adjacencyMatrix = new double[numCities][numCities];
+        cities = new ArrayList<>();
+        for (int i = 0; i < numCities; i++) {
+            for (int j = 0; j < numCities; j++) {
+                if (i == j) {
+                    adjacencyMatrix[i][j] = 0;
+                } else {
+                    adjacencyMatrix[i][j] = Double.POSITIVE_INFINITY;
+                }
+            }
+        }
     }
 
-    public void addCity(City city_id, City city_name)
-    {
-        City city = new City();
-        city.city_id = city_id;
-        city.city_name = city_name;
-        cities[citycount] = city;
-        citycount++;
-    }
-    public void addPath(String path_id, City city1, City city2, double distance)
-    {
-        Path path = new Path(city1,city2,distance);
-        paths[pathcount] = path;
-        pathcount++;
+    public void addCity(City city) {
+        cities.add(city);
     }
 
+    public void addPath(Path path) {
+        int index1 = findCityIndex(path.city1.city_id);
+        int index2 = findCityIndex(path.city2.city_id);
+        if (index1 != -1 && index2 != -1) {
+            adjacencyMatrix[index1][index2] = path.path_distance;
+            adjacencyMatrix[index2][index1] = path.path_distance;
+        }
+    }
 
+    private int findCityIndex(String cityId) {
+        for (int i = 0; i < cities.size(); i++) {
+            if (cities.get(i).city_id.equals(cityId)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
+    public void display() {
+        for (int i = 0; i < cities.size(); i++) {
+            for (int j = 0; j < cities.size(); j++) {
+                System.out.print(adjacencyMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
-class City
-{
+
+class City {
     String city_id;
     String city_name;
+
+    public City(String city_id, String city_name) {
+        this.city_id = city_id;
+        this.city_name = city_name;
+    }
 }
-class Path
-{
+
+class Path {
     String path_id;
     City city1;
     City city2;
-    double distance;
-    public Path(City city1,City city2, double distance)
-    {
-        this.distance=distance;
+    double path_distance;
+    boolean path_isAvailable;
+
+    public Path(City city1, City city2, double distance, boolean isAvailable) {
+        this.city1 = city1;
+        this.city2 = city2;
+        this.path_distance = distance;
+        this.path_isAvailable = isAvailable;
     }
 
+    //Sets Path Availability
+    public void blockPath() {
+        this.path_isAvailable = false;
+    }
+
+    public void unblockPath() {
+        this.path_isAvailable = true;
+    }
 }
